9709ac54e43b03fcce2e3f69c0ee8f7dfde0f938
Patricio Soriano <pasoriano@gmail.com>
2023-01-08 17:14:37 +0100

    1.0 Release HTML output option. Check the options selected in the GUI and update the state of the "Create Report" button

--- project_report/QProjectReport.py
+++ project_report/QProjectReport.py
@@ -3,13 +3,70 @@ import os
 
 from qgis.core import QgsVectorLayer, QgsWkbTypes, QgsRasterLayer
 
+CSS = """
+    <style>
+html,body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe WPC", "Segoe UI",
+          system-ui, "Ubuntu", "Droid Sans", sans-serif;
+        font-size: 14px;
+        line-height: 1.6;
+        padding: 0 26px;
+        word-wrap: break-word;
+      }
 
-def getURL(layer_object):
-    """Static function to get the url from layer source
+      body {
+        padding-top: 1em;
+        padding-bottom: 2em;
+      }
 
-    :param layer_object: QGIS layer
-    :type layer_object: QgsMapLayer
-    """
+      h2,h3,h4,h5,h6 {
+        font-weight: normal;
+        margin-bottom: 0.2em;
+      }
+
+      h1 {
+        padding-bottom: 0.3em;
+        line-height: 1.2;
+        border-bottom-width: 2px;
+        border-bottom-style: solid;
+        font-weight: normal;
+      }
+
+      p {
+        margin-bottom: 0.7em;
+      }
+
+      table {
+        border-collapse: collapse;
+        margin-bottom: 0.7em;
+        
+      }
+
+      th {
+        text-align: left;
+        border-bottom: 1.5px solid;
+      }
+
+      th,td {
+        padding: 1px 10px;
+      }
+      td {
+        word-break: break-word;
+        max-width: 30em;
+        border-top: 0.5px solid;
+      }
+    </style>"""
+
+
+def get_url(layer_object):
+    """Returns the URL of a layer object.
+
+       Parameters:
+       layer_object (QgsVectorLayer or QgsRasterLayer): The layer object to get the URL of.
+
+       Returns:
+       str: The URL of the layer object.
+       """
 
     if isinstance(layer_object, QgsVectorLayer):
         path_url = layer_object.source()
@@ -20,52 +77,87 @@ def getURL(layer_object):
     return path_url
 
 
+def create_table(title, headers, data):
+    """Creates an HTML table from a list of headers and a list of data rows.
+
+    Parameters:
+    title (str): a string for the title
+    headers (list): a list of strings representing the table headers
+    data (list): a list of lists representing the table rows, where each inner list contains the cells for a single row
+
+    Returns:
+    str: an HTML string representing the table
+    """
+
+    table_html = ["<thead><tr>"]
+
+    for cell in headers:
+        table_html.append("<th>{}</th>".format(cell))
+
+    table_html.append("</tr></thead>")
+
+    table_html.append("<tbody>")
+
+    for row in data:
+        table_html.append("<tr>")
+        for cell in row:
+            table_html.append("<td>{}</td>".format(cell))
+        table_html.append("</tr>")
+    table_html.append("</tbody>")
+
+    return "<div><h2>{}</h2><table>{}</table></div>".format(title, "\n".join(table_html))
+
+
 class QProjectReport:
     """ Class with information and properties of QGIS projects and their objects (layers, fields and layouts)
     and generation of output files."""
 
-    def __init__(self, qgsproject, folder):
+    def __init__(self, qgsproject, output_directory):
         """Class Constructor. Generates the attributes relative to the QGIS project.
 
         :param qgsproject: QGIS project
         :type qgsproject: QgsProject object
 
-        :param folder: Directory for creating folders and property files
-        :type folder: str
+        :param output_directory: Directory for creating folders and property files
+        :type output_directory: str
         """
 
         # Project
         self.qgsproject = qgsproject
-        self.folder = folder
+        self.folder = output_directory
         self.project_name = (self.qgsproject.fileName().split('/')[-1]).split('.')[0]
         self.report_directory = os.path.join(self.folder, self.project_name)
         self.csv_directory = os.path.join(self.report_directory, 'csv')
+        self.html_directory = os.path.join(self.report_directory, 'html')
 
         self.project_column_names = ['title',
                                      'fileName',
-                                     'homePath',
+                                     'filePath',
                                      'crs',
                                      'layers_count',
                                      'creationDate',
                                      'lastSaveDate'
                                      ]
-        self.project_data = [
+        self.project_file_path = os.path.split(self.qgsproject.fileName())[0]
+        self.project_file_name = os.path.split(self.qgsproject.fileName())[1]
+        self.project_data = [[
             self.qgsproject.title(),
-            self.qgsproject.fileName(),
-            self.qgsproject.homePath(),
+            self.project_file_name,
+            self.project_file_path,
             self.qgsproject.crs().authid(),
             self.qgsproject.count(),
             self.qgsproject.metadata().creationDateTime().date().toString("yyyy-MM-dd"),
             self.qgsproject.lastSaveDateTime().date().toString("yyyy-MM-dd")
-        ]
+        ]]
 
         # Layers
         self.layers = self.qgsproject.mapLayers().values()
 
-        self.layers_column_names = ['name',
-                                    'crs',
-                                    'path_url',
+        self.layers_column_names = ['id',
+                                    'name',
                                     'storage',
+                                    'path_url',
+                                    'crs',
                                     'encoding',
                                     'geometry_type',
                                     'features_count',
@@ -74,19 +166,17 @@ class QProjectReport:
         self.layers_data = []
 
         # Fields
-        self.layer_fields_column_names = ["layer", "field_name", "display_name", "alias", "comment", "type_name",
-                                          "type",
-                                          "length", "precision"]
+        self.layer_fields_column_names = ["id", "layer", "field_name", "display_name", "alias", "type_name",
+                                          "type", "length"]
         self.layer_fields_data = []
 
-        for layer in self.layers:
+        for index, layer in enumerate(self.layers, start=1):
 
             layer_type = 1 if isinstance(layer, QgsVectorLayer) else 0
-
             layer_name = layer.name()
             crs = layer.crs().authid()
             # path_url = layer.source() if layer_type == 1 else layer.source().split('url=')[1]
-            path_url = getURL(layer)
+            path_url = get_url(layer)
             layer_storage = layer.dataProvider().storageType() if layer_type == 1 else layer.providerType()
             encoding = layer.dataProvider().encoding() if layer_type == 1 else ''
             geometry = QgsWkbTypes.geometryDisplayString(layer.geometryType()) if layer_type == 1 else ''
@@ -94,32 +184,31 @@ class QProjectReport:
             # creationDate = ''
             # lastSaveDate = ''
 
-            self.layers_data.append([layer_name, crs, path_url, layer_storage, encoding, geometry, features])
+            self.layers_data.append([index, layer_name, layer_storage,  path_url, crs, encoding, geometry, features])
 
             if isinstance(layer, QgsVectorLayer):
-                for field in layer.fields():
+                for index, field in enumerate(layer.fields(), start=1):
                     field_name = field.name()
                     display_name = field.displayName()
                     alias = field.alias()
-                    comment = field.comment(),
-
+                    # comment = field.comment(),
                     type_name = field.typeName()
                     field_type = field.type()
                     length = field.length()
-                    precision = field.precision(),
+                    # precision = field.precision(),
 
-                    self.layer_fields_data.append([layer_name, field_name, display_name, alias, comment, type_name,
-                                                   field_type, length, precision])
+                    self.layer_fields_data.append([index, layer_name, field_name, display_name, alias, type_name,
+                                                   field_type, length])
 
         # Layouts
         self.layouts = self.qgsproject.layoutManager().layouts()
 
-        self.layouts_column_names = ['layout_name', 'layout_type', 'atlas', 'atlas_coverageLayer_name']
+        self.layouts_column_names = ['id', 'layout_name', 'layout_type', 'atlas', 'atlas_coverageLayer_name']
         self.layouts_data = []
 
         dict_type_layouts = {0: "PrintLayout", 1: "Report", }
 
-        for layout in self.layouts:
+        for index, layout in enumerate(self.layouts, start=1):
             layout_name = layout.name()
             # project = layout.customProperties()
             layout_type = dict_type_layouts[layout.layoutType()]
@@ -127,7 +216,13 @@ class QProjectReport:
             # atlas_count = layout.atlas().count() if  layout.layoutType() == 0 else ''
             atlas_coverageLayer_name = layout.atlas().coverageLayer().name() if layout.layoutType() == 0 and atlas else ''
 
-            self.layouts_data.append([layout_name, layout_type, atlas, atlas_coverageLayer_name])
+            self.layouts_data.append([index, layout_name, layout_type, atlas, atlas_coverageLayer_name])
+
+        # Check
+        self.check_project = False
+        self.check_layers = False
+        self.check_layouts = False
+        self.check_fields = False
 
     def scaffolding(self):
         """"Making folders structure"""
@@ -135,6 +230,7 @@ class QProjectReport:
         if not os.path.exists(self.report_directory):
             os.mkdir(self.report_directory)
             os.mkdir(self.csv_directory)
+            os.mkdir(self.html_directory)
             print("Directory '% s' created" % self.report_directory)
         else:
             print("Directory '% s' already exists" % self.report_directory)
@@ -186,3 +282,35 @@ class QProjectReport:
             writer.writerow(self.layouts_column_names)
             writer.writerows(self.layouts_data)
             print("CSV layouts created")
+
+    def create_html(self, check_objets):
+        """Create HTML report file"""
+
+        self.check_project = check_objets[0]
+        self.check_layers = check_objets[1]
+        self.check_fields = check_objets[2]
+        self.check_layouts = check_objets[3]
+
+        html_file = os.path.join(self.report_directory, self.html_directory, 'project_report.html')
+        html_title = self.project_data[0][0]
+        html_string = """<html>
+                        <head>{}</head>
+                        <body>
+                        <h1>QGIS Project Report <i>"{}"</i></h1>""".format(CSS, html_title)
+
+        if self.check_project:
+            html_string += create_table('Project', self.project_column_names, self.project_data)
+
+        if self.check_layers:
+            html_string += create_table('Layers', self.layers_column_names, self.layers_data)
+
+        if self.check_fields:
+            html_string += create_table('Fields', self.layer_fields_column_names, self.layer_fields_data)
+
+        if self.check_layouts:
+            html_string += create_table('Layouts', self.layouts_column_names, self.layouts_data)
+
+        html_string += "</body></html>"
+
+        with open(html_file, 'w') as html_file:
+            html_file.write(html_string)
